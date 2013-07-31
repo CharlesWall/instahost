8f0bbf9d56b90257884b609114fe5f214a799cfb
CharlesWall <charles.wall@modea.com>
2013-07-31 01:58:07 -0400

    initial public navigatino

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+
+/*
+ * GET home page.
+ */
+path = require('path');
+fs = require('fs');
+
+var getFileLinks = function(){
+  var ret = [];
+
+  var files = fs.readdirSync('public');
+
+  files.forEach(function(filename){
+    ret.push({ "name": "/" + filename, "href": "/" + filename});
+  });
+
+  console.log(ret);
+
+  return ret;
+};
+
+exports.index = function(req, res){
+  var map = getFileLinks();
+
+  res.render('index', {
+    title: 'Instahost',
+    map: map
+  });
+};
