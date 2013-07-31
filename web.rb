c1032e7d67434f33ca65b794d07a61997e7459c7
CharlesWall <charles.wall@modea.com>
2013-07-31 01:13:47 -0400

    clean

--- web.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra'
-
-use Rack::Auth::Basic, "Restricted Area" do |username, password|
-  [username, password] == ['me', 'myself&I']
-end
-
-get '/' do
-  File.read(File.join('public', 'index.html'))
-end
