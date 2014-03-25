18c7dead2e4f2a15b055ddab24cb786dedc19f3c
CharlesWall <charles.wall@modea.com>
2014-03-25 11:11:35 -0400

    mvp

--- bin/instahost.js
+++ bin/instahost.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 
+var app = require('../app.js');
+
+
 
 
--- routes/index.js
+++ routes/index.js
@@ -5,23 +5,42 @@
 path = require('path');
 fs = require('fs');
 
-var getFileLinks = function(){
-  var ret = [];
 
-  var files = fs.readdirSync('public');
+var hostPath = path.join(process.cwd(), 'public');
+if(!fs.statSync(hostPath)) hostPath = process.cwd();
 
-  files.forEach(function(filename){
-    ret.push({ "name": "/" + filename, "href": "/" + filename});
+var getFileLinks = function(subPath, callback){
+  var links = [];
+  var dir = path.join(hostPath, subPath);
+  console.log(dir);
+  fs.readdir(dir, function(err, files){
+    files.forEach(function(filename){
+      links.push({ "name": "/" + filename, "href": path.join(subPath, filename) });
+    });
+    callback(links);
   });
-
-  return ret;
 };
 
-exports.index = function(req, res){
-  var map = getFileLinks();
+//exports.index = function(req, res){
+//  var map = getFileLinks();
+//
+//  res.render('index', {
+//    title: 'Instahost',
+//    map: map
+//  });
+//};
 
-  res.render('index', {
-    title: 'Instahost',
-    map: map
+exports.dir = function(req, res, next){
+  var filePath = path.join(hostPath, req._parsedUrl.pathname);
+
+  fs.stat(filePath, function(err, stats){
+    if(stats && stats.isDirectory()){
+      getFileLinks(req._parsedUrl.pathname, function(links){
+        res.render('index', {
+          title: 'Instahost',
+          map: links
+        });
+      });
+    } else next();
   });
 };
